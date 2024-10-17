ea2b39d420b7b6848c9ba3fead64e39d98da903d
Davis_the_don <davismugoikou@gmail.com>
2024-10-17 13:08:50 +0300

    added logics for update

--- updateBudget.js
+++ updateBudget.js
@@ -17,10 +17,37 @@ const updateBudget = (titleToUpdate,quantityToUpdate,priceToUpdate)=>{
  const indexOfBudget = budget.findIndex(
    (currentBudget) => currentBudget.title === titleToUpdate,
  );
- //modify the content
+// //check defination
+if(quantityToUpdate == undefined && priceToUpdate == undefined){
+  console.log(chalk.bgBlue("please add content to update "))
+ return;
+}
+else if(quantityToUpdate !=undefined && priceToUpdate == undefined){
+  //update quantity
+  budget[indexOfBudget].quantity = quantityToUpdate;
+  fs.writeFileSync("./Data/Budget.json", JSON.stringify(budget));
+  console.log(chalk.bgGreen("Quantity updated successfully"));
+}
+else if(quantityToUpdate == undefined && priceToUpdate != undefined){
+  //update price
+  budget[indexOfBudget].price = priceToUpdate;
+  fs.writeFileSync("./Data/Budget.json", JSON.stringify(budget));
+  console.log(chalk.bgGreen("price updated successfully"));
+}
+else{
+  //modify the content
  budget[indexOfBudget].quantity = quantityToUpdate;
  budget[indexOfBudget].price = priceToUpdate;
  fs.writeFileSync("./Data/Budget.json", JSON.stringify(budget));
- console.log(chalk.bgGreen("Budget updated successfully!!"));
+ console.log(chalk.bgGreen("Entire budget updated successfully!!"));
+}
+
+
+
+
+
+
+
+ 
 }
 export default updateBudget;
